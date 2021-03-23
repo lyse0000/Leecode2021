548eb4b811f26f29f74edbfc5c082858fa5ba77b
lyse0000 <79489445+lyse0000@users.noreply.github.com>
2021-03-23 13:13:45 -0700

    Create 64. Path.py

---
+++ 64. Path.py
@@ -0,0 +1,21 @@
+# 64. Minimum Path Sum
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        """
+            1   3   1 |      [1   4   5]         1  (4)   5        1   4   5        
+            1   5   1 |                        [(2) [7]  6]        2   7   6
+            4   2   1 |                                            6   8   7  <- ret 7
+
+        """
+        M, N, dp = len(grid), len(grid[0]), grid[0][:]
+        for i in range(1, N): 
+            dp[i] += dp[i-1] 
+        
+        for i in range(1, M):
+            dp[0] += grid[i][0] 
+            for j in range(1, N):
+                #print(dp)
+                dp[j] = min(dp[j], dp[j-1])
+                dp[j]+=grid[i][j]
+    
+        return dp[N-1]
