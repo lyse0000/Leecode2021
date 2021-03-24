e7c4109ba2ab65f13fa6b8fdd62040104f11c396
lyse0000 <79489445+lyse0000@users.noreply.github.com>
2021-03-23 18:47:24 -0700

    Update 62.64. Unique Paths.py

--- 62.64. Unique Paths.py
+++ 62.64. Unique Paths.py
@@ -9,7 +9,23 @@ class Solution:
         return dp[n-1]
 
 
-
+# =====================================================================================================
+# 63. Unique Paths II
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        M, N = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [1]*N
+        for i in range(N):
+            if obstacleGrid[0][i] == 1:
+                dp = [1]*(i) +  [0]*(N-i)
+                break
+        
+        for i in range(1, M): 
+            dp[0] = (1 - obstacleGrid[i][0]) * dp[0]   #<-- (grid[i][0] and dp[upper level]) != 0
+            for j in range(1, N):
+                dp[j] = dp[j]+dp[j-1] if obstacleGrid[i][j]==0 else 0
+            
+        return dp[N-1]
 
 
 # =====================================================================================================
