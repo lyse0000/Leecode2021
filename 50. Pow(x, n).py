95611d4df54ec9ec11a3e9b7e778bb4bbea96d37
lyse0000 <79489445+lyse0000@users.noreply.github.com>
2021-04-07 21:21:44 -0700

    Update 50. Pow(x, n).py

--- 50. Pow(x, n).py
+++ 50. Pow(x, n).py
@@ -14,3 +14,24 @@ class Solution:
         half = self.helper(x, n//2)
 
         return half*half if n%2 == 0 else half*half*x
+    
+# ============================================================================================
+# 69. Sqrt(x)
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x==0: 
+            return 0
+        if 3>=x:
+            return 1
+        
+        low, hi = 2, x//2
+        
+        while low<=hi:
+            mid = (hi+low)//2
+            if mid**2<=x and (mid+1)**2>x:
+                return mid
+            if mid**2>x:
+                hi = mid-1
+            else:
+                low = mid+1
+        return low
